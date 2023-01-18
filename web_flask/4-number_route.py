9a399bfffafcf9770032aa60a4b7b11d46eb7fc2
Ghassen69 <ghassenayari2000@gmail.com>
2023-01-18 16:49:50 +0100

    fxxx

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C_text(text):
+    """ displays text"""
+    return 'C {:s}'.format(text.replace('_', ' '))
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    """ displays text"""
+    return 'Python {:s}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def _number(n):
+    """displays number"""
+    return '{:d} is a number'.format(n)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """diplays Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ display hbnb"""
+    return 'HBNB'
+
+
+if __name__ == "__main__":
+    """ running app.route on 0.0.0.0:(port)5000"""
+    app.run(host='0.0.0.0', port=5000)
