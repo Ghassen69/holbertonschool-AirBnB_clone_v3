640f1c5d7d16ac7f102011c156481a445f470bf9
Ghassen69 <ghassenayari2000@gmail.com>
2023-01-18 16:29:29 +0100

    fx

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,10 +10,12 @@ def hello_hbnb():
     """diplays Hello HBNB!"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ display hbnb"""
     return 'HBNB'
+
 
 if __name__ == "__main__":
     """ running app.route on 0.0.0.0:(port)5000"""
