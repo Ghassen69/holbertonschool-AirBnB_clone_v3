d4faa66e479fdc5d03cfdd5fc6f5fc594d88cb32
Ghassen69 <ghassenayari2000@gmail.com>
2023-01-18 17:03:25 +0100

    fxxx

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -25,7 +25,7 @@ def _number(n):
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number__template(n):
+def _number_template(n):
     """ displays num"""
     return render_template('5-number.html', n=n)
 
